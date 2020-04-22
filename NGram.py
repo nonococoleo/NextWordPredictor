0e0015049a45e33bea9de8335df30b013ea01339
Yufei Gao <ygao6@wpi.edu>
2020-04-21 21:36:40 -0400

    Solved urllib.error.URLError. Expand from Characters N-Grams Model to Words N-Grams Model.

--- NGram.py
+++ NGram.py
@@ -8,11 +8,15 @@ import bs4 as bs
 import urllib.request
 import re
 
+import urllib.request
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 raw_html = urllib.request.urlopen('https://en.wikipedia.org/wiki/Tennis')
 raw_html = raw_html.read()
 
-article_html = bs.BeautifulSoup(raw_html, 'lxml')
+article_html = bs.BeautifulSoup(raw_html, 'lxml')  # if getting bs4.FeatureNotFound, try pip install lxml
 article_paragraphs = article_html.find_all('p')
 article_text = ''
 
@@ -40,6 +44,33 @@ for i in range(200):
     next_char = possible_chars[random.randrange(len(possible_chars))]
     output += next_char
     curr_sequence = output[len(output)-chars:len(output)]
+
+print(output)
+
+
+# 我看了一下最上面comment里那个网站。上面的code好像是using the first n characters to predict the word, 不是predict next word
+
+ngrams = {}
+words = 3
+
+words_tokens = nltk.word_tokenize(article_text)
+for i in range(len(words_tokens)-words):
+    seq = ' '.join(words_tokens[i:i+words])
+    print(seq)
+    if  seq not in ngrams.keys():
+        ngrams[seq] = []
+    ngrams[seq].append(words_tokens[i+words])
+
+curr_sequence = ' '.join(words_tokens[0:words])
+output = curr_sequence
+for i in range(50):
+    if curr_sequence not in ngrams.keys():
+        break
+    possible_words = ngrams[curr_sequence]
+    next_word = possible_words[random.randrange(len(possible_words))]
+    output += ' ' + next_word
+    seq_words = nltk.word_tokenize(output)
+    curr_sequence = ' '.join(seq_words[len(seq_words)-words:len(seq_words)])
 
 print(output)
 
