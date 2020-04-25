49b79dbf696e2d86071807a0c439a68f5b593a4f
Yufei Gao <ygao6@wpi.edu>
2020-04-24 22:40:32 -0400

    Basic NGram model done.

--- NGram.py
+++ NGram.py
@@ -1,76 +1,44 @@
 #https://stackabuse.com/python-for-nlp-developing-an-automatic-text-filler-using-n-grams/
-import nltk
-import numpy as np
+
 import random
-import string
-
-import bs4 as bs
-import urllib.request
-import re
-
-import urllib.request
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-raw_html = urllib.request.urlopen('https://en.wikipedia.org/wiki/Tennis')
-raw_html = raw_html.read()
-
-article_html = bs.BeautifulSoup(raw_html, 'lxml')  # if getting bs4.FeatureNotFound, try pip install lxml
-article_paragraphs = article_html.find_all('p')
-article_text = ''
-
-for para in article_paragraphs:
-    article_text += para.text
-
-article_text = article_text.lower()
-article_text = re.sub(r'[^A-Za-z. ]', '', article_text)
-ngrams = {}
-chars = 3
-
-for i in range(len(article_text)-chars):
-    seq = article_text[i:i+chars]
-    print(seq)
-    if seq not in ngrams.keys():
-        ngrams[seq] = []
-    ngrams[seq].append(article_text[i+chars])
-
-curr_sequence = article_text[0:chars]
-output = curr_sequence
-for i in range(200):
-    if curr_sequence not in ngrams.keys():
-        break
-    possible_chars = ngrams[curr_sequence]
-    next_char = possible_chars[random.randrange(len(possible_chars))]
-    output += next_char
-    curr_sequence = output[len(output)-chars:len(output)]
-
-print(output)
+from data import *
 
 
-# 我看了一下最上面comment里那个网站。上面的code好像是using the first n characters to predict the word, 不是predict next word
+def build_model(n=3, file="corpus/train_business.pkl"):
+    ngrams = {}
+    data = load_data(file)
+    text = ""
+    for lines in data:
+        text += lines + "\n"
+    corpus = clean(text)
+    data, words_tokens = pad(corpus)
+    for i in range(len(words_tokens) - n):
+        seq = ' '.join(words_tokens[i:i + n])
+        if seq not in ngrams.keys():
+            ngrams[seq] = []
+        ngrams[seq].append(words_tokens[i + n])
+    return ngrams
 
-ngrams = {}
-words = 3
 
-words_tokens = nltk.word_tokenize(article_text)
-for i in range(len(words_tokens)-words):
-    seq = ' '.join(words_tokens[i:i+words])
-    print(seq)
-    if  seq not in ngrams.keys():
-        ngrams[seq] = []
-    ngrams[seq].append(words_tokens[i+words])
+def run_model(n=3, ngrams=build_model(), given_words=["Ad","sales","boost"], num_to_predict=50):
+    curr_sequence = ' '.join(given_words[-n:])
+    print()
+    output = curr_sequence
+    for i in range(num_to_predict):
+        if curr_sequence not in ngrams.keys():
+            # todo: find a way to select next word even if the given sequence not exists in model
+            break
+        possible_words = ngrams[curr_sequence]
+        # todo: now is random selection, try to make it probability based
+        next_word = possible_words[random.randrange(len(possible_words))]
+        output += ' ' + next_word
+        seq_words = nltk.word_tokenize(output)
+        curr_sequence = ' '.join(seq_words[len(seq_words) - n:len(seq_words)])
+    print(output)
 
-curr_sequence = ' '.join(words_tokens[0:words])
-output = curr_sequence
-for i in range(50):
-    if curr_sequence not in ngrams.keys():
-        break
-    possible_words = ngrams[curr_sequence]
-    next_word = possible_words[random.randrange(len(possible_words))]
-    output += ' ' + next_word
-    seq_words = nltk.word_tokenize(output)
-    curr_sequence = ' '.join(seq_words[len(seq_words)-words:len(seq_words)])
 
-print(output)
-
+n = 3
+model = build_model(n, "corpus/train_business.pkl")
+given_previous_words = ["May", "last", "year", ","]
+number_of_next_words_to_predict = 50
+run_model(n, model, given_previous_words, number_of_next_words_to_predict)
