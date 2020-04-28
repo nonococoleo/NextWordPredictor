99312f76766cfb350e5bdcca62dc97060d0c01cd
nono <leo212121@foxmail.com>
2020-04-27 21:27:39 -0400

    app v1.1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask_cors import CORS
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, jsonify
 
 import torch
 import numpy as np
@@ -13,39 +13,41 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
 
-with open("vocab", "rb") as f:
+with open("small-vocab", "rb") as f:
     app = load(f)
 
 window_length = 3
-model = torch.load("model_2000_100.pt", map_location=torch.device('cpu'))
+model = torch.load("model_6.pt", map_location=torch.device('cpu'))
 print("model loaded")
 
 server = Flask(__name__)
 CORS(server, supports_credentials=True)
-
+example = []
 
 @server.route('/char', methods=['POST'])
 def responde():
     string = request.form['test']
     example = string.split()
-    if string[-1]!=" ":
+    if string[-1] != " ":
         cur = example.pop().strip()
     else:
-        cur=""
+        cur = ""
+    if len(example) == 0:
+        example.append("<PAD>")
     print(example, cur)
     idx = app.get_idx(example[-window_length:])
     choices = []
-    if len(idx) > 0:
-        test = torch.from_numpy(np.array([idx]))
-        results = model(test.to(device)).detach().cpu().numpy()
 
-        for res in results:
-            temp = res.argsort()[::-1]
-            words = app.get_word(temp)
-            for i in words:
-                if i[:len(cur)] == cur:
-                    choices.append(i)
-    return json.dumps(" ".join(choices[:10]))
+    test = torch.from_numpy(np.array([idx]))
+    results = model(test.to(device)).detach().cpu().numpy()
+
+    for res in results:
+        temp = res.argsort()[::-1]
+        words = app.get_word(temp)
+        for i in words:
+            if i != '<UNK>' and i[:len(cur)] == cur:
+                choices.append(i)
+    return jsonify(dict(enumerate(choices[:10])))
 
 
 @server.route('/')
@@ -55,12 +57,3 @@ def index():
 
 if __name__ == '__main__':
     server.run(host="0.0.0.0", port=8000)
-
-    top_k = 3
-    example = ["<PAD>"] * window_length
-    while True:
-        print(" ".join(example), end=" ")
-        cur = input().strip()
-        example.append(cur)
-
-        # example.append(best)
