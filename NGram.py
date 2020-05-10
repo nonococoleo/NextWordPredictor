c9a2861ac8838d8e0a8e136a9dce91baff61ded7
Yufei Gao <ygao6@wpi.edu>
2020-05-10 15:14:10 -0400

    Added comments.

--- NGram.py
+++ NGram.py
@@ -1,12 +1,11 @@
-# https://stackabuse.com/python-for-nlp-developing-an-automatic-text-filler-using-n-grams/
-# https://www.cs.cornell.edu/courses/cs4740/2014sp/lectures/smoothing+backoff.pdf
-# https://rpubs.com/leomak/TextPrediction_KBO_Katz_Good-Turing
-
 # -*- coding: utf-8 -*-
 import argparse
 from data import *
 
-# Default Values
+# Default prediction output
+MOST_COMMON_WORD = "the"
+
+# Default Parameters for Testing
 n = 3
 training_file = "corpus/train_all.pkl"
 test_file = "corpus/test_all.pkl"
@@ -32,29 +31,33 @@ if args.__dict__["output_file"] is not None:
 if args.__dict__["backoff"] is not None:
     if args.__dict__["backoff"] == "False":
         backoff = False
+# -------------------------
 
-MOST_COMMON_WORD = "the"
+# NGram starts here
 
 
 def build_model(n, file):
     ngrams_backoff = []
+    # tokenize
     data = load_corpus(file)
     text = ""
     for lines in data:
         text += lines + "\n"
     corpus = clean(text)
     data, words_tokens = pad(corpus)
-    vocab_size = len(set(words_tokens))
+    # build 1-gram, 2-gram,..., n-gram
     for cur_n in range(1, n+1):
         ngrams = {}
         for i in range(len(words_tokens) - cur_n):
             seq = ' '.join(words_tokens[i:i + cur_n])
+            # store the sequences
             if seq not in ngrams.keys():
                 ngrams[seq] = {}
             if words_tokens[i + cur_n] not in ngrams[seq].keys():
                 ngrams[seq][words_tokens[i + cur_n]] = 1
             else:
                 ngrams[seq][words_tokens[i + cur_n]] += 1
+        # compute probabilities
         for seq in ngrams.keys():
             count = 0
             for words in ngrams[seq].keys():
@@ -65,7 +68,8 @@ def build_model(n, file):
     return ngrams_backoff
 
 
-def chooseFromDist(pos):
+# select the word that has the highest probability
+def choose_from_dist(pos):
     # pos = {'A': Decimal("0.3"), 'B': Decimal("0.4"), 'C': Decimal("0.3")}
     best_word = MOST_COMMON_WORD
     best_prob = 0
@@ -76,71 +80,67 @@ def chooseFromDist(pos):
     return best_word
 
 
+# use the trained model to predict the next word
 def run_model(n, ngram_backoff, given_words, backoff):
     sequence = ' '.join(given_words[-n:])
+    # if it is a backoff model
     if backoff:
+        # backoff until find an answer
         for ngram in reversed(ngram_backoff):
             if sequence in ngram.keys():
                 possible_words = ngram[sequence]
-                return chooseFromDist(possible_words)
+                return choose_from_dist(possible_words)
             else:
                 sequence = ' '.join(given_words[-(n - 1):])
+    # if it is not a backoff model
     else:
         if sequence in ngram_backoff[-1].keys():
             possible_words = ngram_backoff[-1][sequence]
-            return chooseFromDist(possible_words)
+            return choose_from_dist(possible_words)
     return MOST_COMMON_WORD
 
 
 def test_model(n, ngrams, test_file, backoff):
     test_data = load_corpus(test_file)
     test_text = ""
-    correctCount = 0
-    wrongCount = 0
-    #empty = 0
+    correct_count = 0
+    wrong_count = 0
+    # tokenize
     for lines in test_data:
         test_text += lines + "\n"
     test_corpus = clean(test_text)
     test_data, words_tokens = pad(test_corpus)
-
+    # compare prediction with correct words
     for i in range(len(words_tokens)-n):
-        word = run_model(n,ngrams,words_tokens[i:i+n], backoff)
+        word = run_model(n, ngrams, words_tokens[i:i+n], backoff)
         if words_tokens[i+n] == word:
-            correctCount += 1
+            correct_count += 1
         else:
-            wrongCount += 1
-
+            wrong_count += 1
+    # write output to file
     file = open(output_file, 'a+')
     file.write("N: " + str(n) + "\n")
     file.write("Train File: " + training_file + "\n")
     file.write("Test File: " + test_file+"\n")
     file.write("Backoff: " + str(backoff)+"\n")
     file.write("Total Words: " + str(len(words_tokens) - n)+"\n")
-    file.write("Correct Count: " + str(correctCount)+"\n")
-    file.write("Wrong Count: " + str(wrongCount)+"\n")
-    file.write("Accuracy: " + str(correctCount / (len(words_tokens) - n))+"\n")
+    file.write("Correct Count: " + str(correct_count)+"\n")
+    file.write("Wrong Count: " + str(wrong_count)+"\n")
+    file.write("Accuracy: " + str(correct_count / (len(words_tokens) - n))+"\n")
     file.write("\n")
     print("N: " + str(n))
     print("Train File: " + training_file)
     print("Test File: " + test_file)
     print("Backoff: " + str(backoff))
     print("Total Words: " + str(len(words_tokens)-n))
-    print("Correct Count: " + str(correctCount))
-    print("Wrong Count: " + str(wrongCount))
-    print("Accuracy: " + str(correctCount / (len(words_tokens) - n)))
+    print("Correct Count: " + str(correct_count))
+    print("Wrong Count: " + str(wrong_count))
+    print("Accuracy: " + str(correct_count / (len(words_tokens) - n)))
     print()
 
 
+# train
 model = build_model(n, training_file)
+# test
 test_model(n, model, test_file, backoff)
-
-''' 
-Train File: corpus/train_all.pkl
-N: 1
-Test File: corpus/test_business.pkl
-Total Words: 19475
-Correct Count: 3739
-Wrong Count: 15736
-Accuracy: 0.19198973042362003
-'''
 
--- NGramTest.sh
+++ NGramTest.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
-#for n in {1..4}
-#do
-#    for train in corpus/train_all.pkl corpus/train_business.pkl corpus/train_enter.pkl corpus/train_pol.pkl corpus/train_sport.pkl corpus/train_tech.pkl
-#    do
-#        for test in corpus/test_all.pkl corpus/test_business.pkl corpus/test_enter.pkl corpus/test_pol.pkl corpus/test_sport.pkl corpus/test_tech.pkl
-#        do
-#        python3 NGram.py --N $n --training_file $train --test_file $test --output_file output.txt
-#        done
-#    done
-#done
 
 # Step 1: pick the best N and check performance of backoff model
-#for n in {1..4}
-#do
-#    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file compareN.txt --backoff False
-#    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file compareN.txt --backoff True
-#done
+for n in {1..4}
+do
+    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file compareN.txt --backoff False
+    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file compareN.txt --backoff True
+done
 
 
 #Step 2: comparision between different train&test corpus
