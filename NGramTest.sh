b72275633beea00f5a77526fdf1c043584362bf3
Yufei Gao <ygao6@wpi.edu>
2020-05-10 22:08:05 -0400

    Output to data folder

--- NGramTest.sh
+++ NGramTest.sh
@@ -3,8 +3,8 @@
 # Step 1: pick the best N and check performance of backoff model
 for n in {1..4}
 do
-    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file compareN.txt --backoff False
-    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file compareN.txt --backoff True
+    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file data/compareN.txt --backoff False
+    python3 NGram.py --N $n --training_file corpus/train_all.pkl --test_file corpus/test_all.pkl --output_file data/compareN.txt --backoff True
 done
 
 
@@ -13,6 +13,6 @@ for train in corpus/train_all.pkl corpus/train_business.pkl corpus/train_enter.p
 do
     for test in corpus/test_all.pkl corpus/test_business.pkl corpus/test_enter.pkl corpus/test_pol.pkl corpus/test_sport.pkl corpus/test_tech.pkl
     do
-    python3 NGram.py --N 3 --training_file $train --test_file $test --output_file n3.txt
+    python3 NGram.py --N 3 --training_file $train --test_file $test --output_file data/n3.txt
     done
 done
